67282a82bffbeb64107e8c53ef4f0bbe2db02328
席世龙 <1050201327@qq.com>
2020-02-19 15:00:34 +0800

    初始化仓库

---
+++ src/main/java/com/mmall/concurrency/ConcurrencyApplication.java
@@ -0,0 +1,17 @@
+package com.mmall.concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+public class ConcurrencyApplication extends WebMvcConfigurerAdapter{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyApplication.class, args);
+	}
+
+}
